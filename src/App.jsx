a9642efc309c1153ee2af9450b225f66586fa221
Lmarcha38 <117238874+Lmarcha38@users.noreply.github.com>
2025-03-24 03:45:10 +0000

    new text

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Luke + Ethan (My roomate he said it's cool if I use the picture)</h1>
+      <h1>Luke + Ethan (My roomate, he said it's cool if I use the picture)</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count - 1)}>
           count, but actually it's negative {count}
@@ -26,7 +26,7 @@ function App() {
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        Click on the Luke and Ethan faces to learn more
       </p>
     </>
   )
